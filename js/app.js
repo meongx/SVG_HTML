d92c9974e27d02e033191446f8356474eab4bea0
Khucink Ithem <meongx@gmail.com>
2013-12-13 21:52:25 +0800

    precise grid cell's width and height

--- js/app.js
+++ js/app.js
@@ -1,56 +1,54 @@
-window.vm = {};
+(function() {
+	window.vm = {
+		shapes: [
+			{
+				name: 'Lingkaran',
+				inputs: {
+					x: ko.observable(0),
+					y: ko.observable(0),
+					r: ko.observable(1)
+				},
+				draw: function() {
+					vm.currentShape().clear();
 
-var margin = {top: 20, right: 20, bottom: 30, left: 40},
-    width = window.innerWidth - 40 - margin.left - margin.right,
-    height = window.innerHeight - 100 - margin.top - margin.bottom,
-	scale = 30,
-	domainX = Math.round(width / 2 / scale),
-	domainY = Math.round(height / 2 / scale);
+					var i = vm.currentShape().inputs
+						x = i.x(),
+						y = i.y(),
+						r = i.r(),
+						c = window.graphicObject.append('g');
 
-var scaleX = d3.scale.linear()
-	.domain([-domainX, domainX])
-	.range([0, width]);
+					var circle = c.append('circle')
+						.attr('cx', scaleX(x))
+						.attr('cy', scaleY(y))
+						.attr('r', 1)
+						.attr('class', 'graphic');
+					var centerPoint = c.append('circle')
+						.attr('cx', scaleX(x))
+						.attr('cy', scaleY(y))
+						.attr('r', 3)
+						.attr('class', 'point-center');
 
-var scaleY = d3.scale.linear()
-	.domain([-domainY, domainY])
-	.range([height, 0]);
+					circle.transition()
+						.attr('r',scaleLength(r));
+				},
+				clear: function() {
+					window.graphicObject.selectAll('g').remove();
+				}
+			},
+			{
+				name: 'Persegi',
+			},
+			{
+				name: 'Garis',
+			}
+		],
+		currentShape: ko.observable(undefined)
+	};
 
-var axisX = d3.svg.axis()
-	.scale(scaleX)
-	.orient('bottom')
-	.ticks(width / scale)
-	.tickSize(-height);
+	vm.currentShape.subscribe(function(oldValue) {
+		if (!oldValue) return;
+		oldValue.clear();
+	}, null, 'beforeChange')
 
-var axisY = d3.svg.axis()
-	.scale(scaleY)
-	.orient('left')
-	.ticks(height / scale)
-	.tickSize(-width);
-
-var svg = d3.select('#canvas').append('svg')
-		.attr('width', width + margin.left + margin.right)
-		.attr('height', height + margin.top + margin.bottom)
-	.append('g')
-		.attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
-
-svg.append('rect')
-	.attr('class', 'canvas')
-	.attr('width', width)
-	.attr('height', height);
-
-svg.append('g')
-	.attr('class', 'x axis')
-	.attr('transform', 'translate(0,' + height + ')')
-	.call(axisX);
-
-svg.append('g')
-	.attr('class', 'y axis')
-	.call(axisY);
-
-var zeroFilter = function() { return $(this).text() == '0'; },
-	$zeroTexts = $('#canvas text').filter(zeroFilter),
-	$zeroLines = $zeroTexts.prev();
-
-$zeroLines.each(function() { this.classList.add('zero') });
-
-ko.applyBindings(vm);
+	ko.applyBindings(vm);
+})();
---
+++ js/canvas.js
@@ -0,0 +1,70 @@
+(function() {
+	var margin = { top: 5, right: 5, bottom: 30, left: 20 },
+	    widthWin = window.innerWidth - 20 - margin.left - margin.right,
+	    heightWin = window.innerHeight - 150 - margin.top - margin.bottom,
+	    
+		scaleGrid = 30,
+		width = widthWin - (widthWin % scaleGrid),
+		height = heightWin - (heightWin % scaleGrid),
+
+		domainWidth = width / scaleGrid,
+		minDomainX = (domainWidth - (domainWidth % 2)) / -2,
+		maxDomainX = (domainWidth + (domainWidth % 2)) /  2,
+
+		domainHeight = height / scaleGrid,
+		minDomainY = (domainHeight - (domainHeight % 2)) / -2,
+		maxDomainY = (domainHeight + (domainHeight % 2)) /  2;
+
+	console.log('widthWin',widthWin)
+	console.log('heightWin',heightWin)
+
+	window.scaleX = d3.scale.linear()
+		.domain([minDomainX, maxDomainX])
+		.range([0, width]);
+
+	window.scaleY = d3.scale.linear()
+		.domain([minDomainY, maxDomainY])
+		.range([height, 0]);
+
+	window.scaleLength = function(input) { return input * scaleGrid }
+
+	var axisX = d3.svg.axis()
+		.scale(scaleX)
+		.orient('bottom')
+		.ticks(domainWidth)
+		.tickSize(-height);
+
+	var axisY = d3.svg.axis()
+		.scale(scaleY)
+		.orient('left')
+		.ticks(domainHeight)
+		.tickSize(-width);
+
+	var svg = d3.select('#canvas').append('svg')
+			.attr('width', width + margin.left + margin.right)
+			.attr('height', height + margin.top + margin.bottom)
+		.append('g')
+			.attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+
+	svg.append('rect')
+		.attr('class', 'canvas')
+		.attr('width', width)
+		.attr('height', height);
+
+	svg.append('g')
+		.attr('class', 'x axis')
+		.attr('transform', 'translate(0,' + height + ')')
+		.call(axisX);
+
+	svg.append('g')
+		.attr('class', 'y axis')
+		.call(axisY);
+
+	var zeroFilter = function() { return $(this).text() == '0'; },
+		$zeroTexts = $('#canvas text').filter(zeroFilter),
+		$zeroLines = $zeroTexts.prev();
+
+	$zeroLines.each(function() { this.classList.add('zero') });
+
+	window.graphicObject = svg.append('g');
+})();
